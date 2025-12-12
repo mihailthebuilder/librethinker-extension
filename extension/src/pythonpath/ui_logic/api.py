0eeb9e3654fb531ac458631e7d86937d830b44b6
Mihail Marian <mihailthebuilder@gmail.com>
2025-12-12 21:01:44 +0000

    extension pass right url

--- extension/src/pythonpath/ui_logic/api.py
+++ extension/src/pythonpath/ui_logic/api.py
@@ -3,17 +3,22 @@ import json
 
 
 def get_answer(inputPrompt: str, docText: str, api_key: str | None) -> str:
-    LLM_API_URL = "https://api.librethinker.com/api/v1/responses"
+    url = "https://api.librethinker.com/api/v1/responses/"
 
     body = json.dumps({"prompt": inputPrompt, "text": docText}).encode("utf-8")
     headers = {
         "Content-Type": "application/json",
     }
+
+    append_url = "free"
     if api_key is not None:
         headers["X-Third-Party-Key"] = api_key
+        append_url = "byok"
+
+    url += append_url
 
     req = urllib.request.Request(
-        LLM_API_URL,
+        url,
         data=body,
         method="POST",
         headers=headers,
